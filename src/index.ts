cacaa72689bcfabea5c2c3973e6e9600089d6f30
anna-lisniak <lisniak.anna@gmail.com>
2026-02-13 11:34:01 +0200

    add mock server to listen port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Telegraf } from 'telegraf';
 import { getRandomForesight } from './foresight.js';
+import http from 'http';
 
 const token = process.env.BOT_TOKEN;
 if (!token) {
@@ -44,3 +45,20 @@ bot.on('message', async (ctx) => {
 
 bot.launch(() => console.log('Bot is running'));
 
+
+
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+
+  res.end(JSON.stringify({
+    message: 'Hello from Node.js server',
+  }));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
