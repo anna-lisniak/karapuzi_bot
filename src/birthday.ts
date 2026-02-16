1fca794732078af0f782fa010497aba5e42b38d5
anna-lisniak <lisniak.anna@gmail.com>
2026-02-16 10:38:43 +0200

    Update birthday functionality.

--- src/birthday.ts
+++ src/birthday.ts
@@ -1,5 +1,6 @@
 import type { Context, Telegraf } from "telegraf";
 import type { Update } from "telegraf/types";
+import { getRandom } from "./random.js";
 
 interface User {
     username: string;
@@ -85,27 +86,57 @@ export const processBirthday = async (bot:Telegraf<Context<Update>> ) => {
     console.log('momBirthdays', momBirthdays)
     console.log('childBirthdays', childBirthdays)
 
-    await bot.telegram.sendMessage(
-        -1002122724301,
-        'test'
-    )
+
+
+    let message;
 
     if(momBirthdays.length === 1) { 
-        'Сьогодні ми маємо іменинницю'
+        const mom = momBirthdays[0] as User;
+        message =`✨ Сьогодні День народження у прекрасної @${mom.username}!
+        
+        ${wishes.momWishes.at(getRandom(0, wishes.momWishes.length - 1))}
+        `
     }
 
     if(momBirthdays.length > 1) {
-        'Сьогодні наші іменинниці: '
+        
+        message = `✨ Сьогодні День народження одразу у кількох неймовірних жінок у нашому чаті: ${(momBirthdays as User[]).map((mom) => `@${mom.username}`).join(', ')}!
+        
+        ${wishes.multiMomWishes.at(getRandom(0, wishes.multiMomWishes.length - 1))}
+        `
     }
     
     if(childBirthdays.length === 1) {
-        'Сьогодні святкує день народження наш маленький'
+        const child = childBirthdays[0] as User['child'];
+
+        if(child.gender === 'дівчинка') {
+            message = `🎀 Сьогодні свято в чарівної ${child.name}!
+            
+            ${wishes.babyGirlWishes.at(getRandom(0, wishes.babyGirlWishes.length - 1))}
+            `
+        }
+
+        if(child.gender === 'хлопчик') {
+            message = `🚗 Сьогодні свято в чарівного ${child.name}!
+            
+            ${wishes.babyBoyWishes.at(getRandom(0, wishes.babyBoyWishes.length - 1))}
+            `
+        }
     }
 
     if(childBirthdays.length > 1) {
-        'Сьогодні святкують день народження наші маленькі'
+        
+        message = `🎂 Сьогодні особливий день — одразу кілька Днів народження у наших маленьких карапузиків! 
+        Вітаю ${(childBirthdays as User['child'][]).map((child) => child.name).join(', ')}!
+        
+        ${wishes.multiBabyWishes.at(getRandom(0, wishes.multiBabyWishes.length - 1))}
+        `
     }
-    
 
-
+    if(message) {
+        await bot.telegram.sendMessage(
+            -1002122724301,
+            message
+        )
+    }
 }
